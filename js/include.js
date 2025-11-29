5f6afef5466c6e0d30c6f361b4ddb7b4fb3a5135
hiro-san-163 <funai.h@gmail.com>
2025-11-29 19:01:20 +0900

    Load header and footer HTML via fetch

--- js/include.js
+++ js/include.js
@@ -1,1 +1,14 @@
+// header の読み込み
+fetch("/parts/header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
+
+// footer の読み込み
+fetch("/parts/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
 
