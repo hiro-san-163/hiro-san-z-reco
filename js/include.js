0ae210f09af1f19416ffb5368f2a1330248a6806
hiro-san-163 <funai.h@gmail.com>
2025-12-07 23:07:38 +0900

    Update fetch paths for header and footer includes

--- js/include.js
+++ js/include.js
@@ -1,12 +1,12 @@
 // header の読み込み
-fetch("/parts/header.html")
+fetch("header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
   });
 
 // footer の読み込み
-fetch("/parts/footer.html")
+fetch("footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
